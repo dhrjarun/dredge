3381e61ea1e3219f95d27a184622bdf1abd20ed1
Dhiraj Arun <dhrjarun@gmail.com>
2024-04-11 10:43:15 +0530

    implement node-http and fetch adapter

---
+++ packages/server/source/adapters/fetch.ts
@@ -0,0 +1,143 @@
+import { DredgeApi } from "../_api";
+import {
+  Transformer,
+  AnyRoute,
+  trimSlashes,
+  ResolverResult,
+} from "@dredge/common";
+
+export async function handleFetchRequest<Context extends object = {}>(options: {
+  req: Request;
+  api: DredgeApi<Context, AnyRoute[]>;
+  transformer: Partial<Transformer>;
+  ctx: Context;
+  prefixUrl: URL;
+}): Promise<Response> {
+  const { req, api, transformer, ctx, prefixUrl } = options;
+
+  const url = new URL(req.url!);
+  const initialPathname = trimSlashes(prefixUrl.pathname);
+  if (!url.pathname.startsWith(initialPathname)) {
+    throw "Invalid url";
+  }
+  const path = trimSlashes(url.pathname).slice(initialPathname.length);
+  const data = getDataFromRequestOrResponse(req, {
+    transformer,
+  });
+
+  const headers = Object.fromEntries(req.headers);
+
+  const result = await api.resolveRoute({
+    path,
+    method: req.method,
+    ctx,
+    data,
+    headers,
+    searchParams: url.searchParams,
+  });
+
+  return createResponseFromResolverResult(result, { transformer });
+}
+
+async function getDataFromRequestOrResponse(
+  reqOrRes: {
+    headers: Headers;
+    text: () => Promise<string>;
+    formData: () => Promise<FormData>;
+  },
+  options: { transformer: Partial<Transformer> }
+) {
+  const transformer = populateTransformer(options.transformer);
+  const contentType = reqOrRes.headers.get("Content-Type");
+  let data: any;
+
+  if (contentType?.startsWith("application/json")) {
+    data = transformer.json.deserialize(await reqOrRes.text());
+  }
+  if (contentType?.startsWith("multipart/form-data")) {
+    data = transformer.formData.deserialize(await reqOrRes.formData());
+  }
+
+  if (contentType?.startsWith("application/x-www-form-urlencoded")) {
+    const searchParams = new URLSearchParams(await reqOrRes.text());
+    data = transformer.searchParams.deserialize(searchParams);
+  }
+
+  return Promise.resolve(data);
+}
+
+export function createResponseFromResolverResult(
+  result: ResolverResult<any>,
+  options: {
+    transformer?: Partial<Transformer>;
+  }
+) {
+  const transformer = populateTransformer(options.transformer);
+
+  const { data, ...rest } = result;
+  const contentType = result.headers?.["Content-Type"];
+
+  if (contentType?.startsWith("application/json")) {
+    const json = transformer.json.serialize(data);
+    return new Response(json, {
+      ...rest,
+    });
+  }
+  if (contentType?.startsWith("multipart/form-data")) {
+    const form = transformer.formData.serialize(data);
+    return new Response(form, {
+      ...rest,
+    });
+  }
+
+  if (contentType?.startsWith("application/x-www-form-urlencoded")) {
+    const searchParams = new URLSearchParams(data);
+    const form = transformer.searchParams.serialize(searchParams);
+    return new Response(form, {
+      ...rest,
+    });
+  }
+
+  return new Response(data, {
+    ...rest,
+  });
+}
+
+function populateTransformer(
+  transformer: Partial<Transformer> = {}
+): Transformer {
+  const _transformer: Transformer = {
+    json: {
+      serialize: JSON.stringify,
+      deserialize: JSON.parse,
+    },
+    formData: {
+      serialize: (object) => {
+        const formData = new FormData();
+        Object.entries(object).forEach(([key, value]) => {
+          if (typeof value === "string" || value instanceof Blob) {
+            formData.append(key, value);
+          } else {
+            throw "serialization failed";
+          }
+        });
+
+        return formData;
+      },
+      deserialize: (object) => {
+        return Object.fromEntries(object.entries());
+      },
+    },
+    searchParams: {
+      serialize: (object) => new URLSearchParams(object),
+      deserialize: (object) => Object.fromEntries(object.entries()),
+    },
+  };
+  Object.entries(transformer).forEach(([key, value]) => {
+    if (value) {
+      (_transformer as any)[key] = value;
+    }
+  });
+
+  return _transformer;
+}
--- packages/server/source/adapters/node-http.ts
+++ packages/server/source/adapters/node-http.ts
@@ -1,62 +1,227 @@
 import type * as http from "http";
-import { DredgeApi, executeRoute } from "../api";
-import { AnyRoute } from "../route";
+import { DredgeApi } from "../_api";
+import { Transformer, AnyRoute, trimSlashes } from "@dredge/common";
+import busboy from "busboy";
+import { FormData, File } from "formdata-node";
+import { FormDataEncoder } from "form-data-encoder";
+import { Readable } from "stream";
 
-export interface DataTransformer {
-  serialize(object: any): any;
-  deserialize(object: any): any;
-}
-
-// TODO
-// endpoint
-export function createNodeHttpAdapter(options: {
-  api: DredgeApi<AnyRoute>;
-  bodyTransformer: DataTransformer;
-  ctx: object;
+export function createNodeHttpRequestHandler<
+  Context extends object = {}
+>(options: {
+  api: DredgeApi<Context, AnyRoute[]>;
+  transformer: Partial<Transformer>;
+  ctx: Context;
+  prefixUrl: URL;
 }) {
-  const { bodyTransformer, api, ctx } = options;
-  const root = api._root;
+  const { transformer, api, ctx, prefixUrl } = options;
 
   return async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    const method = req.method || "get";
-    const url = new URL(req.url || "");
-    const path = url.pathname as "";
-    const searchParams = url.searchParams;
-    const headers = req.headers;
-    const rawBody = getBody(req);
-    const body = bodyTransformer.deserialize(rawBody);
+    const url = new URL(req.url!);
+    const initialPathname = trimSlashes(prefixUrl.pathname);
+    if (url.pathname.startsWith(initialPathname)) {
+      throw "Invalid url";
+    }
+    const path = trimSlashes(url.pathname).slice(initialPathname.length);
+    const body = getRequestBody(req, { transformer });
+    const data = body.data();
 
-    const result = await executeRoute(root, ctx, {
-      method,
+    const headers = req.headers;
+
+    const result = await api.resolveRoute({
+      ctx,
+      method: req.method,
+      searchParams: url.searchParams,
       path,
-      searchParams,
-      body,
+      data,
       headers,
     });
 
-    res.statusCode = result.status;
-    res.statusMessage = result.statusText;
-    for (const item in result.headers) {
-      res.setHeader(item, result.headers[item]);
-    }
-
-    const responseBody = bodyTransformer.serialize(result.body);
-    res.write(responseBody);
+    res.writeHead(result.status, result.statusText, result.headers);
+    writeDataIntoResponse(res, data, {
+      transformer,
+    });
     res.end();
   };
 }
 
-function getBody(request) {
-  return new Promise((resolve) => {
-    const bodyChunks: any[] = [];
-    let body;
-    request
-      .on("data", (chunk) => {
-        bodyChunks.push(chunk);
-      })
-      .on("end", () => {
-        body = Buffer.concat(bodyChunks).toString();
-        resolve(body);
+export function writeDataIntoResponse(
+  res: http.ServerResponse,
+  data: any,
+  options: {
+    callback?: ((error: Error | null | undefined) => void) | undefined;
+    transformer?: Partial<Transformer>;
+  }
+) {
+  const { transformer: _transformer = {}, callback } = options;
+  const transformer = populateTransformer(options.transformer);
+
+  const contentType = res.getHeader("Content-Type") as string;
+
+  if (contentType?.startsWith("application/json")) {
+    const json = transformer.json.serialize(data);
+    return res.write(json, callback);
+  }
+  if (contentType?.startsWith("multipart/form-data")) {
+    const form = transformer.formData.serialize(data);
+    const encoder = new FormDataEncoder(form);
+    return res.write(Readable.from(encoder.encode()), callback);
+  }
+
+  if (contentType?.startsWith("application/x-www-form-urlencoded")) {
+    const searchParams = new URLSearchParams(data);
+    const form = transformer.searchParams.serialize(searchParams);
+    return res.write(form.toString(), callback);
+  }
+
+  return res.write(data, callback);
+}
+
+export function getRequestBody(
+  req: http.IncomingMessage,
+  options: { transformer?: Partial<Transformer> }
+) {
+  const transformer = populateTransformer(options.transformer);
+
+  function buffer() {
+    return new Promise<Buffer>((resolve, reject) => {
+      const bodyChunks: any[] = [];
+      let body: Buffer;
+      req
+        .on("error", (err) => {
+          reject(err);
+        })
+        .on("data", (chunk) => {
+          bodyChunks.push(chunk);
+        })
+        .on("end", () => {
+          body = Buffer.concat(bodyChunks);
+          resolve(body);
+        });
+    });
+  }
+
+  function text() {
+    return new Promise<string>((resolve, reject) => {
+      buffer()
+        .then((buffer) => {
+          resolve(buffer.toString());
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  function formData() {
+    const bb = busboy({ headers: req.headers });
+
+    return new Promise<FormData>((resolve, reject) => {
+      const formData = new FormData();
+      bb.on("field", (name, value) => {
+        formData.append(name, value);
       });
+
+      bb.on("file", (name, stream, info) => {
+        const chunks: any[] = [];
+        stream
+          .on("data", (chunk) => {
+            chunks.push(chunk);
+          })
+          .on("error", (err) => {
+            reject(err);
+          })
+          .on("close", () => {
+            formData.append(
+              name,
+              new File(chunks, info.filename, {
+                type: info.mimeType,
+              })
+            );
+          });
+      });
+
+      bb.on("close", () => {
+        resolve(formData);
+      });
+
+      bb.on("error", (err) => reject(err));
+
+      req.pipe(bb);
+    });
+  }
+
+  function data() {
+    return new Promise<unknown>(async (resolve, reject) => {
+      const contentType = req.headers["content-type"];
+
+      try {
+        let data: unknown;
+
+        if (contentType?.startsWith("application/json")) {
+          data = transformer.json.deserialize(await text());
+        }
+
+        if (contentType?.startsWith("multipart/form-data")) {
+          data = transformer.formData.deserialize(await formData());
+        }
+
+        if (contentType?.startsWith("application/x-www-form-urlencoded")) {
+          const searchParams = new URLSearchParams(await text());
+          data = transformer.searchParams.deserialize(searchParams);
+        }
+
+        resolve(data);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  return {
+    buffer,
+    text,
+    formData,
+    data,
+  };
+}
+
+function populateTransformer(
+  transformer: Partial<Transformer> = {}
+): Transformer {
+  const _transformer: Transformer = {
+    json: {
+      serialize: JSON.stringify,
+      deserialize: JSON.parse,
+    },
+    formData: {
+      serialize: (object) => {
+        const formData = new FormData();
+        Object.entries(object).forEach(([key, value]) => {
+          if (typeof value === "string" || value instanceof Blob) {
+            formData.append(key, value);
+          } else {
+            throw "serialization failed";
+          }
+        });
+
+        return formData;
+      },
+      deserialize: (object) => {
+        return Object.fromEntries(object.entries());
+      },
+    },
+    searchParams: {
+      serialize: (object) => new URLSearchParams(object),
+      deserialize: (object) => Object.fromEntries(object.entries()),
+    },
+  };
+
+  Object.entries(transformer).forEach(([key, value]) => {
+    if (value) {
+      (_transformer as any)[key] = value;
+    }
   });
+
+  return _transformer;
 }
