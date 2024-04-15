e2837503246fcf8c05ca6d350baf52b584a499b2
Dhiraj Arun <dhrjarun@gmail.com>
2024-04-15 11:45:09 +0530

    refactor types, client, routes, api

---
+++ packages/server/source/index.ts
@@ -0,0 +1,20 @@
+// TODO
+// add types to resolveRoute
+// make data() in ResolverResult return Promise
+// refactor fetchClient option type
+// create dredgeFetch instance creator
+
+// test resolveRoute
+// test adapters
+// test dredge-fetch client
+// delete outdated codes
+// better error objects
+// add more options for dredgeFetch
+// fix adapter type problems
+
+// setup tsup
+// setup eslint, prettier
+// add Readme.MD, CONTRIBUTION.md
+// publish to npm
+
+// setup the docs site with astro
